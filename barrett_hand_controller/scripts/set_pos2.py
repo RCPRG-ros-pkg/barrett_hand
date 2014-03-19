3c0a3264332873585439b43dae174f433b8098eb
Dawid Seredyński <dawid.seredynski@gmail.com>
2014-03-19 15:33:21 +0100

    fix in script

--- barrett_hand_controller/scripts/set_pos2.py
+++ barrett_hand_controller/scripts/set_pos2.py
@@ -3,43 +3,14 @@
 import roslib; roslib.load_manifest('barrett_hand_controller')
 import rospy
 import sys
-from barrett_hand_controller.msg import *
+from barrett_hand_controller_srvs.msg import *
 from barrett_hand_controller_srvs.srv import *
-
-def move_hand(prefix, spread, f1, f2, f3):
-
-        global pub
-	cmd = BHCmd()
-	cmd.header.seq = 0
-        cmd.header.frame_id = ""
-        cmd.header.stamp = rospy.Time.now()
-	cmd.cmd[0] = f1*3.14
-	cmd.cmd[1] = f2*3.14
-	cmd.cmd[2] = f3*3.14
-	cmd.cmd[3] = spread*3.14
-        pub.publish(cmd)
-#	print cmd
 
 def move_hand_client(prefix, f1, f2, f3, spread):
     rospy.wait_for_service('/' + prefix + '_hand/move_hand')
     try:
         move_hand = rospy.ServiceProxy('/' + prefix + '_hand/move_hand', BHMoveHand)
-	cmd = BHMoveHand()
-	cmd.f1 = f1
-	cmd.f2 = f2
-	cmd.f3 = f3
-	cmd.sp = spread
-	cmd.f1_speed = 0.7
-	cmd.f2_speed = 0.7
-	cmd.f3_speed = 0.7
-	cmd.sp_speed = 0.7
-	cmd.f1_torque = 1000
-	cmd.f2_torque = 1000
-	cmd.f3_torque = 1000
-	cmd.sp_torque = 1000
         resp1 = move_hand(f1*3.14, f2*3.14, f3*3.14, spread*3.14, 0.7, 0.7, 0.7, 0.7, 1000, 1000, 1000, 1000)
-#        resp1 = move_hand(cmd)
-#        return resp1.result
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
@@ -55,10 +26,6 @@ if __name__ == '__main__':
 
 	if 6 == len(a):
             move_hand_client(a[1], float(a[2]), float(a[3]), float(a[4]), float(a[5]))
-#            pub = rospy.Publisher('/' + a[1] + '_hand/BHCmdx', BHCmd)
-#            move_hand(float(a[2]), float(a[3]), float(a[4]), float(a[5]))
-#            rospy.sleep(1)
-#            move_hand(float(a[2]), float(a[3]), float(a[4]), float(a[5]))
         else:
             print "Usage: %s prefix f1 f2 f3 spread"%a[0]
 
